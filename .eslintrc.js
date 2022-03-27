9f8694f8a8264e6728a1622baaf0ee934f37a3ce
50ra4 <50ra4.dev.dev@gmail.com>
2022-03-27 19:04:01 +0900

    change: .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
-  plugins: ['prettier'],
-  extends: ['plugin:react/recommended'],
+  root: true, // プロジェクトのルートに配置していると教えている
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser', // ESLintにTypeScriptを適応
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'], // TypeScriptプラグインのルールを適用
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
   rules: {
     'prettier/prettier': 'error',
   },
