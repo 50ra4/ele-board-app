ef80854c93689305feb1cd52c7fe538e790f3687
50ra4 <50ra4.dev.dev@gmail.com>
2022-03-29 00:36:52 +0900

    add: global css

---
+++ src/presentation/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle, css } from 'styled-components';
+import { StyledComponentsTheme } from './theme';
+
+const customCss = (theme: StyledComponentsTheme) => css`
+  li {
+    list-style: none;
+  }
+
+  *,
+  ::before,
+  ::after {
+    box-sizing: border-box;
+    border-style: solid;
+    border-width: 0;
+  }
+
+  * {
+    font-family: ${theme.font.family};
+  }
+
+  body {
+    line-height: 1;
+    color: ${theme.color.default.font};
+    background-color: ${theme.color.default.background};
+  }
+
+  p {
+    word-break: break-all;
+    white-space: pre-line;
+  }
+
+  button,
+  input {
+    padding: 0;
+    margin: 0;
+    appearance: none;
+    cursor: pointer;
+    &:disabled {
+      cursor: default;
+    }
+  }
+
+  select {
+    appearance: none;
+    &:-ms-expand {
+      display: none;
+    }
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle<{ theme: StyledComponentsTheme }>`
+  ${({ theme }) => customCss(theme)}
+`;
