8ab6c47dc89f7b96d9bc8107637221c1f44fca1b
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-03 16:10:45 +0900

    refactor: PasswordInput

--- src/presentation/components/inputs/PasswordInput.tsx
+++ src/presentation/components/inputs/PasswordInput.tsx
@@ -1,43 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useToggle } from 'src/hooks/useToggle';
-import { IconButton } from '../IconButton/IconButton';
-import { VisibilityIcon } from '../icons/VisibilityIcon';
 import { InputBase, InputBaseProps } from './InputBase';
+import { VisibilityButton } from './VisibilityButton';
 
 type PasswordInputProps = Omit<InputBaseProps, 'type'>;
 
 function UnstyledPasswordInput({ className, value = '', ...otherProps }: PasswordInputProps) {
   const [visible, toggleVisible] = useToggle(false);
-
   return (
     <div className={className}>
       <InputBase {...otherProps} type={visible ? 'text' : 'password'} value={value} />
-      <VisibilityButton visible={visible} onClick={toggleVisible} />
+      <VisibilityButton
+        label={visible ? 'パスワードを非表示にする' : 'パスワードを表示する'}
+        visible={visible}
+        onClick={toggleVisible}
+      />
     </div>
   );
 }
-
-function UnstyledVisibilityButton({
-  className,
-  visible,
-  onClick,
-}: {
-  className?: string;
-  visible: boolean;
-  onClick: () => void;
-}) {
-  return (
-    <IconButton className={className} onClick={onClick}>
-      <VisibilityIcon
-        size="medium"
-        titleAccess={visible ? 'パスワードを非表示にする' : 'パスワードを表示する'}
-      />
-    </IconButton>
-  );
-}
-
-const VisibilityButton = styled(UnstyledVisibilityButton)``;
 
 export const PasswordInput = styled(UnstyledPasswordInput)`
   width: 100%;
