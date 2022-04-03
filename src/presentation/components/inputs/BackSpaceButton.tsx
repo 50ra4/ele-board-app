a0b7b5d27c3650ef6a8fba01a5a12dc93d19c7c4
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-03 16:05:18 +0900

    add: BackSpaceButton

---
+++ src/presentation/components/inputs/BackSpaceButton.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { IconButton } from '../IconButton/IconButton';
+import { BackSpaceIcon } from '../icons/BackSpaceIcon';
+
+export type BackSpaceButtonProps = {
+  className?: string;
+  areaLabel: string;
+  onClick: () => void;
+};
+
+function UnstyledBackSpaceButton({ className, areaLabel, onClick }: BackSpaceButtonProps) {
+  return (
+    <IconButton className={className} onClick={onClick}>
+      <BackSpaceIcon size="medium" titleAccess={areaLabel} />
+    </IconButton>
+  );
+}
+
+export const BackSpaceButton = styled(UnstyledBackSpaceButton)``;
