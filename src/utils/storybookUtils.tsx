8b5f7c760edefc606395ba916bb9792006c7cf77
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-16 15:32:36 +0900

    update: add functions to storybook utils

--- src/utils/storybookUtils.tsx
+++ src/utils/storybookUtils.tsx
@@ -1,17 +1,25 @@
 import React, { ComponentProps, ComponentType } from 'react';
-import type { ComponentStoryObj as _ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import type {
+  ComponentStoryObj as _ComponentStoryObj,
+  ComponentMeta as _ComponentMeta,
+  ComponentStoryFn,
+} from '@storybook/react';
 
-export const createStoryMeta = <T extends React.FC<any>>(
+export const createStoryMeta = <T extends React.VFC<any>>(
   Component: T,
-  { title, ...params }: { title: string } & Omit<ComponentMeta<T>, 'title' | 'component'>,
-): ComponentMeta<T> => ({
+  { title, ...params }: { title: string } & Omit<_ComponentMeta<T>, 'title' | 'component'>,
+): _ComponentMeta<T> => ({
   title: title,
   component: Component,
   ...params,
 });
 
+export type ComponentStoryObj<P extends Record<string, unknown>> = _ComponentStoryObj<
+  ComponentType<P>
+>;
+
 export const createStoryDocs = <
-  Component extends React.FC<any>,
+  Component extends React.VFC<any>,
   Props extends Record<string, unknown> = ComponentProps<Component>,
 >(
   _: Component,
@@ -19,6 +27,21 @@ export const createStoryDocs = <
   obj: Omit<_ComponentStoryObj<Component>, 'args'> = {},
 ) => ({ ...obj, args: props });
 
-export type ComponentStoryObj<P extends Record<string, unknown>> = _ComponentStoryObj<
-  ComponentType<P>
->;
+export const createComponentMeta = <
+  Props extends Record<string, unknown>,
+  Component extends React.VFC<Props> = React.VFC<Props>,
+>(
+  component: Component,
+  { title, ...params }: { title: string } & Omit<_ComponentMeta<Component>, 'title' | 'component'>,
+): _ComponentMeta<Component> => ({
+  ...params,
+  title,
+  component,
+});
+
+export const createStoryTemplate =
+  <Props extends Record<string, unknown>, C extends React.VFC<any> = React.VFC<Props>>(
+    Component: C,
+  ): ComponentStoryFn<C> =>
+  (args) =>
+    <Component {...args} />;
