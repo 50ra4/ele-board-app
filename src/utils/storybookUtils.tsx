4559d1ea81d64d4f2336f9206f922d091391ffe3
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-16 16:14:37 +0900

    change: remove no-use storybook utils

--- src/utils/storybookUtils.tsx
+++ src/utils/storybookUtils.tsx
@@ -1,8 +1,7 @@
-import React, { ComponentProps, ComponentType } from 'react';
+import React, { ComponentType } from 'react';
 import type {
   ComponentStoryObj as _ComponentStoryObj,
   ComponentMeta as _ComponentMeta,
-  ComponentStoryFn,
 } from '@storybook/react';
 
 export const createStoryMeta = <T extends React.VFC<any>>(
@@ -17,31 +16,3 @@ export const createStoryMeta = <T extends React.VFC<any>>(
 export type ComponentStoryObj<P extends Record<string, unknown>> = _ComponentStoryObj<
   ComponentType<P>
 >;
-
-export const createStoryDocs = <
-  Component extends React.VFC<any>,
-  Props extends Record<string, unknown> = ComponentProps<Component>,
->(
-  _: Component,
-  props: Props,
-  obj: Omit<_ComponentStoryObj<Component>, 'args'> = {},
-) => ({ ...obj, args: props });
-
-export const createComponentMeta = <
-  Props extends Record<string, unknown>,
-  Component extends React.VFC<Props> = React.VFC<Props>,
->(
-  component: Component,
-  { title, ...params }: { title: string } & Omit<_ComponentMeta<Component>, 'title' | 'component'>,
-): _ComponentMeta<Component> => ({
-  ...params,
-  title,
-  component,
-});
-
-export const createStoryTemplate =
-  <Props extends Record<string, unknown>, C extends React.VFC<any> = React.VFC<Props>>(
-    Component: C,
-  ): ComponentStoryFn<C> =>
-  (args) =>
-    <Component {...args} />;
