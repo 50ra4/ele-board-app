37c3933b13161324fa660dd38e5d5fe86a17c4cc
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-16 19:25:13 +0900

    add: SnackbarProvider stories

---
+++ src/presentation/components/Snackbar/SnackbarProvider.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { createStoryMeta } from 'src/utils/storybookUtils';
+import { TextButton } from '../TextButton/TextButton';
+import { SnackbarProvider, useSnackBar } from './SnackbarProvider';
+
+export default createStoryMeta(SnackbarProvider, {
+  title: 'Snackbar/SnackbarProvider',
+});
+
+const PlayButtons = () => {
+  const { enqueue } = useSnackBar();
+
+  const onFadeOut = (id: string) => {
+    console.log(`fadeout:${id}`);
+  };
+
+  return (
+    <div>
+      <TextButton
+        color="primary"
+        text="info"
+        onClick={() => {
+          enqueue('info', { severity: 'info', onFadeOut });
+        }}
+      />
+      <TextButton
+        color="primary"
+        text="success"
+        onClick={() => {
+          enqueue('success', { severity: 'success', onFadeOut });
+        }}
+      />
+      <TextButton
+        color="primary"
+        text="Popup"
+        onClick={() => {
+          enqueue('warning', { severity: 'warning', onFadeOut });
+        }}
+      />
+      <TextButton
+        color="primary"
+        text="Popup"
+        onClick={() => {
+          enqueue('error', { severity: 'error', duration: 10000, onFadeOut });
+        }}
+      />
+    </div>
+  );
+};
+
+export const Play = () => {
+  return (
+    <SnackbarProvider>
+      <PlayButtons />
+    </SnackbarProvider>
+  );
+};
