62ef44391d46fc67ba0926bca34be628947d6f38
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-24 21:46:15 +0900

    apply: MailLoginForm to login page

--- src/pages/private/login.tsx
+++ src/pages/private/login.tsx
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router';
 import { LoginMethod, LoginSelectCard } from 'src/presentation/parts/auth/LoginSelectCard';
+import { MailLoginForm } from 'src/presentation/parts/auth/MailLoginForm';
 import { TemplateNo1 } from 'src/presentation/templates/TemplateNo1';
 import styled from 'styled-components';
 
 function LoginPage() {
   const router = useRouter();
+
+  const showMailLoginForm = router.query?.type === 'mail';
+
+  const onSubmit = (value: { email: string; password: string }) => {
+    // TODO:
+  };
 
   const onSelect = (method: LoginMethod) => {
     if (method === 'mail') {
@@ -15,14 +22,25 @@ function LoginPage() {
 
   return (
     <TemplateNo1>
-      <StyledLoginSelectCard onSelect={onSelect} />
+      {showMailLoginForm ? (
+        <StyledMailLoginForm onSubmit={onSubmit} />
+      ) : (
+        <StyledLoginSelectCard onSelect={onSelect} />
+      )}
     </TemplateNo1>
   );
 }
 
 const StyledLoginSelectCard = styled(LoginSelectCard)`
   margin-top: 100px;
-  max-width: 400px;
+  width: 100%;
+  max-width: 480px;
+`;
+
+const StyledMailLoginForm = styled(MailLoginForm)`
+  margin-top: 100px;
+  width: 100%;
+  max-width: 480px;
 `;
 
 export default LoginPage;
