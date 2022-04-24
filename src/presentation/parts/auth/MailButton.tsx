b678ef0669bb9f09d6890316ebd4b2301fb243ee
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-24 19:30:18 +0900

    add: MailButton

---
+++ src/presentation/parts/auth/MailButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { MailIcon } from '@/components/icons/MailIcon';
+import { Button } from '@/components/inputs/Button/Button';
+import { SvgIcon } from '@/components/icons/SvgIcon';
+
+type Props = {
+  className?: string;
+  text: string;
+  onClick: () => void;
+  isLoading?: boolean;
+  disabled?: boolean;
+};
+
+export const MailButton = styled(
+  React.memo(function MailButton({ className, text, onClick }: Props) {
+    return (
+      <Button className={className} color="primary" onClick={onClick}>
+        <div>
+          <MailIcon />
+          <span>{text}</span>
+        </div>
+      </Button>
+    );
+  }),
+)`
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > ${SvgIcon} {
+      width: 18px;
+      height: 18px;
+    }
+    & > span {
+      margin-left: 4px;
+    }
+  }
+`;
