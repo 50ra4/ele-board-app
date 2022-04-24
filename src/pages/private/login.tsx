35ceedb20ee217f9868b70c131afe10bad061da6
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-24 22:04:37 +0900

    add: description to login page

--- src/pages/private/login.tsx
+++ src/pages/private/login.tsx
@@ -1,8 +1,11 @@
+import styled from 'styled-components';
 import { useRouter } from 'next/router';
+
+import { OutlineDescription } from '@/components/display/OutlineDescription/OutlineDescription';
 import { LoginMethod, LoginSelectCard } from 'src/presentation/parts/auth/LoginSelectCard';
 import { MailLoginForm } from 'src/presentation/parts/auth/MailLoginForm';
 import { TemplateNo1 } from 'src/presentation/templates/TemplateNo1';
-import styled from 'styled-components';
+import { Link } from '@/components/navigation/Link/Link';
 
 function LoginPage() {
   const router = useRouter();
@@ -16,6 +19,7 @@ function LoginPage() {
   const onSelect = (method: LoginMethod) => {
     if (method === 'mail') {
       router.push({ href: router.pathname, query: { type: 'mail' } });
+      return;
     }
     console.error(`${method} is not implemented.`);
   };
@@ -23,10 +27,24 @@ function LoginPage() {
   return (
     <TemplateNo1>
       {showMailLoginForm ? (
-        <StyledMailLoginForm onSubmit={onSubmit} />
+        <>
+          <StyledMailLoginForm onSubmit={onSubmit} />
+          <StyledOutlineDescription>
+            パスワードをお忘れの方は
+            <Link href="/private/resetPassword" text="パスワードリセット" />
+          </StyledOutlineDescription>
+          <StyledOutlineDescription>
+            他の方法で
+            <Link href="/private/login" text="ログイン" />
+          </StyledOutlineDescription>
+        </>
       ) : (
         <StyledLoginSelectCard onSelect={onSelect} />
       )}
+      <StyledOutlineDescription>
+        初めての方は
+        <Link href="/private/signUp" text="アカウント登録" />
+      </StyledOutlineDescription>
     </TemplateNo1>
   );
 }
@@ -40,6 +58,11 @@ const StyledLoginSelectCard = styled(LoginSelectCard)`
 const StyledMailLoginForm = styled(MailLoginForm)`
   margin-top: 100px;
   width: 100%;
+  max-width: 480px;
+`;
+
+const StyledOutlineDescription = styled(OutlineDescription)`
+  margin-top: 16px;
   max-width: 480px;
 `;
 
