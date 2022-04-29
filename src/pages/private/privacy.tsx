3e94a56eabf45eea68b903133c015733c4681700
50ra4 <50ra4.dev.dev@gmail.com>
2022-04-29 17:46:17 +0900

    add: PrivacyPage and TermsPage

---
+++ src/pages/private/privacy.tsx
@@ -0,0 +1,7 @@
+import { NotImplementedTemplate } from 'src/presentation/templates/NotImplemented';
+
+function PrivacyPage() {
+  return <NotImplementedTemplate />;
+}
+
+export default PrivacyPage;
---
+++ src/pages/private/terms.tsx
@@ -0,0 +1,7 @@
+import { NotImplementedTemplate } from 'src/presentation/templates/NotImplemented';
+
+function TermsPage() {
+  return <NotImplementedTemplate />;
+}
+
+export default TermsPage;
